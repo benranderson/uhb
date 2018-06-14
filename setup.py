e4e3462a1ddbce24080d0f99a099ef9cf2d5ed10
Ben Randerson <ben.m.randerson@gmail.com>
2018-06-14 21:05:56 +0100

    refined setup.py

--- setup.py
+++ setup.py
@@ -7,31 +7,19 @@ import uhb
 
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
-
-requirements = [
-    "click",
-    "scipy",
-    "matplotlib",
-]
-
-setup_requirements = ["pytest-runner"]
-
-test_requirements = ["pytest"]
 
 setup(
     name="uhb",
     version=uhb.__version__,
-    description="Upheaval Buckling Tools",
-    long_description=readme,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Ben Randerson",
     author_email="ben.m.randerson@gmail.com",
     url="https://github.com/benranderson/uhb",
     packages=find_packages(include=["uhb"]),
     entry_points={"console_scripts": ["uhb=uhb.cli:main"]},
+    install_requires=open("requirements.txt").readlines(),
     include_package_data=True,
-    install_requires=requirements,
     license="MIT License",
     zip_safe=False,
     keywords="upheaval buckling uhb engineering pipelines",
@@ -40,11 +28,8 @@ setup(
         "Intended Audience :: Other Audience",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
-    test_suite="tests",
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
 )
--- uhb/__init__.py
+++ uhb/__init__.py
@@ -1,3 +1,3 @@
 __author__ = """Ben Randerson"""
 __email__ = "ben.m.randerson@email.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
